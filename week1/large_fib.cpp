b336e69fa0ed061b64b753320751f4619efbe237
Vladimir Petrigo <taenaru@gmail.com>
2015-11-03 00:08:41 +0300

    add comments for week 1, task 3

--- week1/large_fib.cpp
+++ week1/large_fib.cpp
@@ -10,32 +10,31 @@
 
 #include <iostream>
 #include <vector>
-#include <iterator>
+#include "Prints.hpp"
 
 using namespace std;
 
 constexpr int f_1 = 0;
 constexpr int f_2 = 1;
 
-void print_vector(const vector<int>& v) {
-  ostream_iterator<int> os{cout, " "};
-  copy(v.begin(), v.end(), os);
-  cout << endl;
-}
-
+// Because of modulo operation under the Fibonacci numbers causes to periodical
+// sequence (https://en.wikipedia.org/wiki/Pisano_period) it is possible to
+// calculate untill n-th Fibonacci number reached or sequence starts again from F(k) = 0, F(k + 1) = 1
 int large_fib_rem(long long n, int m) {
   vector<int> fib_num {f_1, f_2};
   int cnt = 1;
 
+  print_vector(fib_num);
   for (int i = 2; i < n; ++i) {
     fib_num.push_back((fib_num[i - 2] + fib_num[i - 1]) % m);
+    // if the sequence repeats then exit the loop
     if (fib_num[i] == 1 && fib_num[i - 1] == 0) {
       break;
     }
 
     ++cnt;
   }
-
+  print_vector(fib_num);
   auto offset = n % cnt;
 
   return fib_num[offset];
