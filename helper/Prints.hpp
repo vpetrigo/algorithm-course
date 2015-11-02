366bf455c807a37362489a0d35b9884aa8f2dd68
Vladimir Petrigo <taenaru@gmail.com>
2015-11-03 00:07:53 +0300

    add helper functions header

---
+++ helper/Prints.hpp
@@ -0,0 +1,20 @@
+//
+// Created by vpetrigo
+//
+
+#ifndef ALGORITHM_COURSE_PRINTS_H
+#define ALGORITHM_COURSE_PRINTS_H
+
+#include <iostream>
+#include <iterator>
+
+template <typename Cont>
+void print_vector(const Cont& v) {
+  using Cont_type = typename Cont::value_type;
+
+  std::ostream_iterator<Cont_type> os{std::cout, " "};
+  std::copy(v.begin(), v.end(), os);
+  std::cout << std::endl;
+}
+
+#endif //ALGORITHM_COURSE_PRINTS_H
