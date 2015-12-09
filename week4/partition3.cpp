68c717f273e897503248b74191a4aa9c18ecf805
Vladimir Petrigo <taenaru@gmail.com>
2015-12-09 15:35:47 +0300

    fix answer restore function for 3-partition

--- week4/partition3.cpp
+++ week4/partition3.cpp
@@ -6,18 +6,24 @@
 #include <vector>
 #include <numeric>
 #include <limits>
+#include <set>
+#include <iomanip>
 
 using namespace std;
 
 class Partition3 {
+ private:
+  void print_partition(const vector<int>& S);
  public:
-  static constexpr int inf = numeric_limits<int>::min();
+  static constexpr int inf = -1;
 
   void init_storage(int n, int size);
 
   int partition(const vector<int> &A, int i, int sum1, int sum2);
 
   void print_storage() const;
+
+  void restore_answer(const vector<int>& A, int sum);
 
   vector<vector<vector<int>>> storage;
 };
@@ -40,7 +46,7 @@ int main() {
     auto A = sum / 3;
     p3.init_storage(v.size(), A + 1);
     cout << p3.partition(v, v.size() - 1, A, A) << endl;
-    p3.print_storage();
+    p3.restore_answer(v, A);
   }
   else {
     cout << "0";
@@ -50,7 +56,7 @@ int main() {
 }
 
 int Partition3::partition(const vector<int> &A, int i, int sum1, int sum2) {
-  if (storage[i][sum1][sum2] == -1) {
+  if (storage[i][sum1][sum2] == inf) {
     if (sum1 == 0 && sum2 == 0) {
       storage[i][sum1][sum2] = 1;
     }
@@ -96,4 +102,54 @@ void Partition3::print_storage() const {
     cout << "-------------------------" << endl;
     cout << endl;
   }
+}
+
+void Partition3::restore_answer(const vector<int> &A, int sum) {
+  int j = sum;
+  int k = sum;
+
+  vector<int> A1;
+  vector<int> A2;
+  vector<int> A3;
+
+  for (int i = A.size() - 1; i >= 0; --i) {
+    if (storage[i][j][k] != inf) {
+      if (A[i] <= j) {
+        if (i == 0 || storage[i - 1][j - A[i]][k] != inf) {
+          A1.push_back(A[i]);
+          j -= A[i];
+        }
+      }
+      else if (A[i] <= k) {
+        if (i == 0 || storage[i - 1][j][k  - A[i]] != inf) {
+          A2.push_back(A[i]);
+          k -= A[i];
+        }
+      }
+      else {
+        A3.push_back(A[i]);
+      }
+    }
+    else {
+      A3.push_back(A[i]);
+    }
+  }
+
+  print_partition(A1);
+  print_partition(A2);
+  print_partition(A3);
+}
+
+void Partition3::print_partition(const vector<int> &S) {
+  const auto length = S.size();
+  cout << "{";
+
+  for (auto i = 0; i < length; ++i) {
+    cout << S[i];
+    if (i != length - 1) {
+      cout << ", ";
+    }
+  }
+
+  cout << "}" << endl;
 }
